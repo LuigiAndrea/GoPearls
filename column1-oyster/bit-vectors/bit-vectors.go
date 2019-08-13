d8217dbff2103c7647a8baf32f4a22c354f5cf36
LAD <luigiandrea@gmail.com>
2019-08-12 23:47:17 -0400

    add tests for bit-vector

--- column1-oyster/bit-vectors/bit-vectors.go
+++ column1-oyster/bit-vectors/bit-vectors.go
@@ -1,20 +1,28 @@
 package column1
 
+import "errors"
+
 type bitVector []int
 
 var bitsPerWord = 32
 
-func newBitVector(n int) *bitVector {
+func newBitVector(n int) (bv *bitVector, err error) {
+	if n < 0 {
+		return bv, errors.New("Negative n argument in newBitVector")
+	}
+
 	size := (n / bitsPerWord) + 1
 	bitVector := make(bitVector, size)
-	return &bitVector
+	return &bitVector, err
 }
 
 func (bit bitVector) get(index int) byte {
 	word := bit[index/bitsPerWord]
+
 	if word&(1<<uint(index%bitsPerWord)) > 0 {
 		return 1
 	}
+
 	return 0
 }
 
---
+++ column1-oyster/bit-vectors/bit-vectors_test.go
@@ -0,0 +1,56 @@
+// +build all column1 bitvector
+
+package column1
+
+import (
+	"testing"
+)
+
+func TestBitVectorOperations(t *testing.T) {
+	bitVectors, _ := newBitVector(5000000)
+
+	setValues := []int{500000, 500001, 500005}
+
+	for _, v := range setValues {
+		bitVectors.set(v)
+	}
+
+	getValues := []int{500000, 500001, 500005, 500002, 0}
+	expectedGetValues := []byte{1, 1, 1, 0, 0}
+
+	for i := 0; i < len(getValues); i++ {
+		getValue := bitVectors.get(getValues[i])
+		if getValue != expectedGetValues[i] {
+			t.Errorf("\nExpected value %d - Actual value %d", expectedGetValues[i], getValue)
+		}
+	}
+
+	for _, v := range getValues {
+		bitVectors.clear(v)
+	}
+
+	for _, v := range getValues {
+		clearValue := bitVectors.get(v)
+		if clearValue != 0 {
+			t.Errorf("\nExpected value %d - Actual value %d", 0, clearValue)
+		}
+	}
+}
+
+func TestBitVectorLength(t *testing.T) {
+
+	testValues := []int{500000, 31, 0, 32, 2000000, 2000031}
+	expectedValues := []int{15626, 1, 1, 2, 62501, 62501}
+
+	if _, err := newBitVector(-20); err == nil {
+		t.Errorf("\nExpected an expection: negative n argument in newBitVector")
+	}
+
+	for i := 0; i < len(testValues); i++ {
+		bitVector, _ := newBitVector(testValues[i])
+		lenBitVector := len(*bitVector)
+		if lenBitVector != expectedValues[i] {
+			t.Errorf("\nExpected value %d - Actual value %d", expectedValues[i], lenBitVector)
+		}
+	}
+}
