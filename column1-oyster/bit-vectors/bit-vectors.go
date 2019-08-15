054e602d73f9efcc14c46b640b134ebb1276d585
LAD <luigiandrea@gmail.com>
2019-08-14 23:43:56 -0400

    add tests for get in bit vectors

--- column1-oyster/bit-vectors/bit-vectors.go
+++ column1-oyster/bit-vectors/bit-vectors.go
@@ -1,29 +1,38 @@
 package column1
 
-import "errors"
+import (
+	"errors"
+	"fmt"
+)
 
 type bitVector []int
 
 var bitsPerWord = 32
+var size = 0
 
 func newBitVector(n int) (bv *bitVector, err error) {
 	if n < 0 {
 		return bv, errors.New("Negative n argument in newBitVector")
 	}
 
-	size := (n / bitsPerWord) + 1
+	size = (n / bitsPerWord) + 1
 	bitVector := make(bitVector, size)
 	return &bitVector, err
 }
 
-func (bit bitVector) get(index int) byte {
+func (bit bitVector) get(index int) (byte, error) {
+
+	if err := bit.validateInput(index); err != nil {
+		return 0, err
+	}
+
 	word := bit[index/bitsPerWord]
 
 	if word&(1<<uint(index%bitsPerWord)) > 0 {
-		return 1
+		return 1, nil
 	}
 
-	return 0
+	return 0, nil
 }
 
 func (bit bitVector) set(index int) {
@@ -32,4 +41,11 @@ func (bit bitVector) set(index int) {
 
 func (bit bitVector) clear(index int) {
 	bit[index/bitsPerWord] &= ^(1 << uint(index%bitsPerWord))
+}
+
+func (bit bitVector) validateInput(index int) error {
+	if index < 0 || index >= size*bitsPerWord {
+		return fmt.Errorf("Out of Range index argument")
+	}
+	return nil
 }
--- column1-oyster/bit-vectors/bit-vectors_test.go
+++ column1-oyster/bit-vectors/bit-vectors_test.go
@@ -8,7 +8,6 @@ import (
 
 func TestBitVectorOperations(t *testing.T) {
 	bitVectors, _ := newBitVector(5000000)
-
 	setValues := []int{500000, 500001, 500005}
 
 	for _, v := range setValues {
@@ -19,7 +18,7 @@ func TestBitVectorOperations(t *testing.T) {
 	expectedGetValues := []byte{1, 1, 1, 0, 0}
 
 	for i := 0; i < len(getValues); i++ {
-		getValue := bitVectors.get(getValues[i])
+		getValue, _ := bitVectors.get(getValues[i])
 		if getValue != expectedGetValues[i] {
 			t.Errorf("\nExpected value %d - Actual value %d", expectedGetValues[i], getValue)
 		}
@@ -30,9 +29,22 @@ func TestBitVectorOperations(t *testing.T) {
 	}
 
 	for _, v := range getValues {
-		clearValue := bitVectors.get(v)
+		clearValue, _ := bitVectors.get(v)
 		if clearValue != 0 {
 			t.Errorf("\nExpected value %d - Actual value %d", 0, clearValue)
+		}
+	}
+}
+
+func TestBitVectorGetOperationWrongValue(t *testing.T) {
+	testValue := []int{-200, -2, 50000}
+	bitVectors, _ := newBitVector(5000)
+
+	for i, v := range testValue {
+		if _, err := bitVectors.get(v); err == nil {
+			t.Errorf("Expected an Out of Range error")
+		} else {
+			t.Logf("%d: %v", i, err.Error())
 		}
 	}
 }
