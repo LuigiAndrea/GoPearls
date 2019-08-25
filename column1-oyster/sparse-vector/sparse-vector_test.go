8cd0e95bba5f8dbdbff4f22497f47c9590990746
LAD <luigiandrea@gmail.com>
2019-08-25 01:45:44 -0400

    add generic test for sparse vector

---
+++ column1-oyster/sparse-vector/sparse-vector_test.go
@@ -0,0 +1,37 @@
+// +build all column1 sparseVector
+
+package sparsevector
+
+import (
+	"testing"
+)
+
+func TestSparseVector(t *testing.T) {
+	sparseVector, _ := NewSparseVector(10)
+	expectedValues := map[int]int{3: 51, 2: 50, 9: 12, 8: 0}
+	expectedInitializedElement := len(expectedValues)
+	var numberOfInitializedElement int
+
+	sparseVector.add(3, 51)
+	sparseVector.add(2, 5)
+	sparseVector.add(9, 12)
+	sparseVector.add(2, 50)
+	sparseVector.add(8, 0)
+
+	for i := 0; i < sparseVector.length; i++ {
+		if num, err := sparseVector.get(i); err == nil {
+			numberOfInitializedElement++
+			if num != expectedValues[i] {
+				t.Errorf("Expected value %d - Actual value %d", expectedValues[i], num)
+			} else {
+				t.Logf("(Index: %d, Num: %d)", i, num)
+			}
+		}
+	}
+
+	if numberOfInitializedElement != len(expectedValues) {
+		t.Errorf("Expected value %d - Actual value %d", expectedInitializedElement, numberOfInitializedElement)
+	} else {
+		t.Logf("%d elements have been initialized", numberOfInitializedElement)
+	}
+}
