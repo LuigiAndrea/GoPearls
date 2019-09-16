9356207bb9ee607984ea4a979a05ff428586bbcd
LAD <luigiandrea@gmail.com>
2019-09-16 02:35:04 -0400

    add check if the value exist in the list

--- column2-aha/atleast-twice/atleast-twice.go
+++ column2-aha/atleast-twice/atleast-twice.go
@@ -2,11 +2,15 @@ package twice
 
 //find in a signed sequential list (or file) an integer that appears at least twice
 
-//Return the number in the sequential list that appear at least twice
-func searchAtLeastTwiceValue(list []int) int {
+//Return the number in the sequential list that appear at least twice, return false if it doesn't exist
+func searchAtLeastTwiceValue(list []int) (bool, int) {
 	n := len(list)
 	midPoint := n / 2
-	return searchValue(list, 0, n-1, midPoint, n)
+	res := searchValue(list, 0, n-1, midPoint, n)
+	if res != n {
+		return true, res
+	}
+	return false, res
 }
 
 func searchValue(list []int, left, right, midpoint, currentTwice int) int {
