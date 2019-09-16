d55f40b4ce9025fdfdc3cce9c6bd7e3af342a5dc
LAD <luigiandrea@gmail.com>
2019-09-16 02:28:41 -0400

    add search for value that appear at least twice sequential list

--- column2-aha/atleast-twice/atleast-twice.go
+++ column2-aha/atleast-twice/atleast-twice.go
@@ -2,14 +2,23 @@ package twice
 
 //find in a signed sequential list (or file) an integer that appears at least twice
 
-func atleastTwice() {
-
+//Return the number in the sequential list that appear at least twice
+func searchAtLeastTwiceValue(list []int) int {
+	n := len(list)
+	midPoint := n / 2
+	return searchValue(list, 0, n-1, midPoint, n)
 }
 
-func populateSequentialList(list []int, n int) {
-	odd := n % 2
-	midPoint := n / 2
-	for i := -midPoint - odd; i < midPoint; i++ {
-		list[i+midPoint+odd] = i
+func searchValue(list []int, left, right, midpoint, currentTwice int) int {
+	if left > right {
+		return currentTwice
 	}
+
+	i := (left + right) / 2
+	if i-midpoint == list[i] {
+		// check right half
+		return searchValue(list, i+1, right, midpoint, currentTwice)
+	}
+	//check left half
+	return searchValue(list, left, i-1, midpoint, list[i])
 }
