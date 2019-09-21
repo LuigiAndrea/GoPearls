071fa7447f85841e1541f8b3cd4d932d6059a956
LAD <luigiandrea@gmail.com>
2019-09-21 04:12:59 -0400

    add kelement subset with QuickSelect

--- column2-aha/k-element-subset/k-element-subset.go
+++ column2-aha/k-element-subset/k-element-subset.go
@@ -1,20 +1,69 @@
 package ksubset
 
-import "sort"
+import (
+	"math/rand"
+	"sort"
+	"time"
+)
 
 //Given a set of n real numbers, a real number t, and an integer k,
 // how quickly can you determine whether there exists a k-element subset of the set that sums to at most t?
 
 // n*log(n), true if the k-element subset exists
-func existKSubsetSumToT(list []int, t, k int) bool {
+func existKSubset(list []int, t, k int) bool {
 	listToCheck := sort.IntSlice(list)
 	listToCheck.Sort()
 	result := sumElements(listToCheck[:k])
 	if result <= t {
 		return true
 	}
-
 	return false
+}
+
+func existKSubsetQuickSelect(list []int, t, k int) bool {
+	subsetKQuickSelect(list, 0, len(list)-1, k)
+	result := sumElements(list[:k])
+
+	if result <= t {
+		return true
+	}
+	return false
+}
+
+func subsetKQuickSelect(list []int, left, right, k int) {
+	index := partition(list, left, right)
+	currentSmallestElemnts := index + 1 - left
+
+	if currentSmallestElemnts == k {
+		return
+	}
+
+	if currentSmallestElemnts > k {
+		subsetKQuickSelect(list, left, index-1, k)
+	} else {
+		subsetKQuickSelect(list, index+1, right, k-currentSmallestElemnts)
+	}
+}
+
+//Partition the list in half using a random pivot, return the index of the pivot
+func partition(list []int, left, right int) int {
+	rand.Seed(time.Now().UnixNano())
+	pos := rand.Intn(right-left) + left
+	pivot := list[pos]
+
+	intSlice := sort.IntSlice(list)
+	intSlice.Swap(pos, right)
+
+	i := left
+	for j := left; j < right; j++ {
+		if intSlice[j] < pivot {
+			intSlice.Swap(i, j)
+			i++
+		}
+	}
+
+	intSlice.Swap(i, right)
+	return i
 }
 
 func sumElements(elements []int) int {
