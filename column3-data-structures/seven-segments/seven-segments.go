94b9c1ed4be15ee0ea7e821c046df3255c70de08
LAD <luigiandrea@gmail.com>
2019-09-29 12:53:48 -0400

    add seven-segment problem

---
+++ column3-data-structures/seven-segments/seven-segments.go
@@ -0,0 +1,71 @@
+package segments
+
+import (
+	"math"
+)
+
+//segments
+//   2_
+// 3|1_|4
+// 5|0_|6
+var segments [7]byte
+
+func displayNumber(number int16) [5]byte {
+	var display [5]byte
+	buildSegments()
+	i := len(display) - 1
+	for number > 0 {
+		display[i] = encodeDigit(number % 10)
+		number /= 10
+		i--
+	}
+
+	return display
+}
+
+func buildSegments() {
+	for i := 0; i < 7; i++ {
+		segments[i] = 1 << uint(i)
+	}
+}
+
+func encodeDigit(digit int16) byte {
+	var encodedDigit byte
+	switch digit {
+	case 0:
+		encodedDigit = allSegmentsON() &^ activeSegments(1)
+	case 1:
+		encodedDigit = activeSegments(4, 6)
+	case 2:
+		encodedDigit = allSegmentsON() &^ activeSegments(3, 6)
+	case 3:
+		encodedDigit = allSegmentsON() &^ activeSegments(3, 5)
+	case 4:
+		encodedDigit = allSegmentsON() &^ activeSegments(0, 2, 5)
+	case 5:
+		encodedDigit = allSegmentsON() &^ activeSegments(4, 5)
+	case 6:
+		encodedDigit = allSegmentsON() &^ activeSegments(4)
+	case 7:
+		encodedDigit = activeSegments(2, 4, 6)
+	case 8:
+		encodedDigit = allSegmentsON()
+	case 9:
+		encodedDigit = allSegmentsON() &^ activeSegments(0, 5)
+
+	}
+
+	return encodedDigit
+}
+
+func allSegmentsON() byte {
+	return byte(math.Exp2(7) - 1)
+}
+
+func activeSegments(segmentNumber ...int) byte {
+	var activeSegment byte
+	for _, v := range segmentNumber {
+		activeSegment |= segments[v]
+	}
+	return activeSegment
+}
