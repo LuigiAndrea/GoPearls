747904261dd5f432c128587325550bfd4e7013f7
LAD <luigiandrea@gmail.com>
2019-12-03 23:27:39 -0500

    Add GetDayOfWeek func

--- column3-data-structures/date-problems/date-problems.go
+++ column3-data-structures/date-problems/date-problems.go
@@ -2,11 +2,17 @@ package datepearls
 
 import "time"
 
-func numberDaysBetweenTwoDatesGoWay(date1, date2 time.Time) int {
+//NumberDaysBetweenTwoDatesGoWay Compute the number of days between two dates
+func NumberDaysBetweenTwoDatesGoWay(date1, date2 time.Time) int {
 
 	if date1.After(date2) {
 		date1, date2 = date2, date1
 	}
 
 	return int(date2.Sub(date1).Hours() / 24)
+}
+
+//GetDayOfWeek Given a date, returns its day of the week
+func GetDayOfWeek(Date time.Time) time.Weekday {
+	return Date.Weekday()
 }
