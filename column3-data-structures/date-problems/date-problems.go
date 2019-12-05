44b963da429d1280290d0008f7a9ad7212572b75
LAD <luigiandrea@gmail.com>
2019-12-04 23:43:42 -0500

    add func to Compute a Calendar parameters: year and month

--- column3-data-structures/date-problems/date-problems.go
+++ column3-data-structures/date-problems/date-problems.go
@@ -1,9 +1,11 @@
 package datepearls
 
-import "time"
+import (
+	"time"
+)
 
-//NumberDaysBetweenTwoDatesGoWay Compute the number of days between two dates
-func NumberDaysBetweenTwoDatesGoWay(date1, date2 time.Time) int {
+//NumberDaysBetweenTwoDates Compute the number of days between two dates
+func NumberDaysBetweenTwoDates(date1, date2 time.Time) int {
 
 	if date1.After(date2) {
 		date1, date2 = date2, date1
@@ -15,4 +17,18 @@ func NumberDaysBetweenTwoDatesGoWay(date1, date2 time.Time) int {
 //GetDayOfWeek Given a date, returns its day of the week
 func GetDayOfWeek(Date time.Time) time.Weekday {
 	return Date.Weekday()
+}
+
+//ComputeCalendar Produce a calendar for the month
+func ComputeCalendar(Year int, Month time.Month) []time.Weekday {
+	dataToCompute := time.Date(Year, Month, 1, 0, 0, 0, 0, time.UTC)
+	days := NumberDaysBetweenTwoDates(dataToCompute, time.Date(Year, Month+1, 1, 0, 0, 0, 0, time.UTC))
+	weekDayValue := GetDayOfWeek(dataToCompute)
+	r := make([]time.Weekday, 0)
+
+	for i := 0; i < days; i++ {
+		r = append(r, time.Weekday((int(weekDayValue)+i)%7))
+	}
+
+	return r
 }
