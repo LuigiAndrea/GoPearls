d6954235c22139a7156d82f0e7c32278e58921cd
LAD <luigiandrea@gmail.com>
2020-01-29 23:46:12 -0500

    fix search at least twice sequential list add more tests

--- column2-aha/atleast-twice/atleast-twice.go
+++ column2-aha/atleast-twice/atleast-twice.go
@@ -1,28 +1,35 @@
 package twice
 
-//find in a signed sequential list (or file) an integer that appears at least twice
+// Find in a signed sequential list (or file) an integer that appears at least twice.
 
 //Return the number in the sequential list that appear at least twice, return false if it doesn't exist
+//Assume the list is sequential
 func searchAtLeastTwiceValue(list []int) (bool, int) {
 	n := len(list)
-	midPoint := n / 2
-	res := searchValue(list, 0, n-1, midPoint, n)
+	var res int
+
+	if n != 0 {
+		res = searchValue(list, 0, n-1, list[0], n)
+	} else {
+		res = searchValue(list, 0, n-1, 0, n)
+	}
+
 	if res != n {
 		return true, res
 	}
 	return false, res
 }
 
-func searchValue(list []int, left, right, midpoint, currentTwice int) int {
+func searchValue(list []int, left, right, startValue, currentTwice int) int {
 	if left > right {
 		return currentTwice
 	}
 
 	i := (left + right) / 2
-	if i-midpoint == list[i] {
+	if list[i] == startValue+i {
 		// check right half
-		return searchValue(list, i+1, right, midpoint, currentTwice)
+		return searchValue(list, i+1, right, startValue, currentTwice)
 	}
 	//check left half
-	return searchValue(list, left, i-1, midpoint, list[i])
+	return searchValue(list, left, i-1, startValue, list[i])
 }
--- column2-aha/atleast-twice/atleast-twice_test.go
+++ column2-aha/atleast-twice/atleast-twice_test.go
@@ -28,8 +28,6 @@ func TestAtLeastTwice(t *testing.T) {
 		if ok, v := searchAtLeastTwiceValue(list); ok {
 			if v != test.expectedValue {
 				t.Errorf("\nExpected value: '%d' - Actual value '%d' \nList: %#v", test.expectedValue, v, list)
-			} else {
-				t.Logf("Repeated element: '%d' \nList: %#v", v, list)
 			}
 		} else {
 			t.Errorf("Repeated element not detected \nList: %#v", list)
@@ -57,6 +55,33 @@ func TestEdgeCases(t *testing.T) {
 	}
 }
 
+func TestAtLeastTwiceGeneral(t *testing.T) {
+
+	type testData struct {
+		list          []int
+		expectedValue int
+	}
+
+	tests := []testData{
+		testData{list: []int{0, 0, 1, 2, 3, 4, 5}, expectedValue: 0},
+		testData{list: []int{-1, 0, 1, 1, 1, 2, 2, 2, 3, 4, 5, 5}, expectedValue: 1},
+		testData{list: []int{2, 2, 2}, expectedValue: 2},
+		testData{list: []int{10, 11, 12, 13, 13}, expectedValue: 13},
+	}
+
+	for _, test := range tests {
+
+		if ok, v := searchAtLeastTwiceValue(test.list); ok {
+			if v != test.expectedValue {
+				t.Errorf("\nExpected value: '%d' - Actual value '%d' \nList: %#v", test.expectedValue, v, test.list)
+			}
+		} else {
+			t.Errorf("Repeated element not detected \nList: %#v", test.list)
+		}
+	}
+
+}
+
 type skipIndexes map[int]struct{}
 
 func (s skipIndexes) contains(index int) bool {
@@ -75,7 +100,7 @@ func populateSequentialList(list []int, n int, indexes skipIndexes) {
 		idx := i + midPoint
 		if indexes != nil && idx != 0 && indexes.contains(idx) {
 			list[i+midPoint] = i + repeated - 1
-			repeated -= 1
+			repeated--
 			continue
 		}
 
