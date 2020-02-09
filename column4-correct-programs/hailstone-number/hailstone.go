645c5a505a1fec07b7ac11ac39a773967c1be756
LAD <luigiandrea@gmail.com>
2020-02-08 22:26:21 -0500

    add hailstone recursive

--- column4-correct-programs/hailstone-number/hailstone.go
+++ column4-correct-programs/hailstone-number/hailstone.go
@@ -16,6 +16,22 @@ func hailstoneSequence(x int) []int {
 	return result
 }
 
+func hailstoneRecursive(x int, result []int) []int {
+	if x == 1 {
+		return result
+	}
+
+	if even(x) {
+		x /= 2
+		result = append(result, x)
+		return hailstoneRecursive(x, result)
+	}
+
+	x = x*3 + 1
+	result = append(result, x)
+	return hailstoneRecursive(x, result)
+}
+
 func even(n int) bool {
 	if n%2 == 0 {
 		return true
--- column4-correct-programs/hailstone-number/hailstone_test.go
+++ column4-correct-programs/hailstone-number/hailstone_test.go
@@ -7,8 +7,9 @@ import (
 	"testing"
 )
 
-func TestTerminate(t *testing.T) {
+func TestHailstoneSequenceTerminate(t *testing.T) {
 	for _, v := range []int{17, 3, 99, 77, 30, 104} {
 		fmt.Printf("%3d --> %3v\n", v, hailstoneSequence(v))
+		fmt.Printf("%3d --> %3v\n", v, hailstoneRecursive(v, []int{}))
 	}
 }
