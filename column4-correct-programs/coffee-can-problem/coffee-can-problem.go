298155368c83cff5e137c52621fa03ced9bf259e
LAD <luigiandrea@gmail.com>
2020-02-10 01:26:52 -0500

    Add coffeeCanProblem

---
+++ column4-correct-programs/coffee-can-problem/coffee-can-problem.go
@@ -0,0 +1,52 @@
+// You are initially given a coffee can that contains some black beans and some white beans and a large pile of “extra” black beans.
+// You then repeat the following process until there is a single bean left in the can.
+// Randomly select two beans from the can. If they are the same color, throw them both out and insert an extra black bean.
+// If they are different colors, return the white bean to the can and throw out the black.
+
+package coffeebean
+
+import (
+	"math/rand"
+	"time"
+)
+
+type bean struct {
+	color string
+}
+
+func coffeeCanBeans(coffeeCan []bean) bean {
+
+	for size := len(coffeeCan); size > 2; size-- {
+		rand.Seed(time.Now().UnixNano())
+		posA, posB := rand.Intn(size), rand.Intn(size-1)
+		for posB == posA {
+			posB = rand.Intn(size - 1)
+		}
+		removeBeans(&coffeeCan, posA, posB)
+	}
+	removeBeans(&coffeeCan, 0, 1)
+	return coffeeCan[0]
+}
+
+func haveSameColor(A, B bean) bool {
+	return A == B
+}
+
+func removeBeans(coffeeCan *[]bean, posA, posB int) {
+	beanA, beanB := (*coffeeCan)[posA], (*coffeeCan)[posB]
+	if haveSameColor(beanA, beanB) {
+		*coffeeCan = append(*coffeeCan, bean{color: "Black"})
+		delete(coffeeCan, posA)
+		delete(coffeeCan, posB)
+	} else if beanA.color == "Black" {
+		delete(coffeeCan, posA)
+	} else {
+		delete(coffeeCan, posB)
+	}
+}
+
+func delete(coffeeCan *[]bean, pos int) {
+	size := len(*coffeeCan) - 1
+	(*coffeeCan)[pos] = (*coffeeCan)[size]
+	*coffeeCan = (*coffeeCan)[:size]
+}
