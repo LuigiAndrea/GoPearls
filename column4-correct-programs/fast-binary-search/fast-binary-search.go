6020571f4d12127b7bf2038870f4491d9cb7e8e7
LAD <luigiandrea@gmail.com>
2020-03-13 23:42:14 -0400

    add fast binary search vers 1

---
+++ column4-correct-programs/fast-binary-search/fast-binary-search.go
@@ -0,0 +1,21 @@
+package fastbinary
+
+func fastSearch(a []int, v int) (p int) {
+	n := len(a)
+	u := n
+	l := -1
+	for l+1 < u {
+		m := (l + u) / 2
+		if a[m] < v {
+			l = m
+		} else {
+			u = m
+		}
+	}
+	if u == n || a[u] != v {
+		p = -1
+	}
+
+	p = u
+	return
+}
