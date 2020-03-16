caa08fc19dd72059d4509a0ebebb829242634c5f
LAD <luigiandrea@gmail.com>
2020-03-15 23:50:41 -0400

    fast binary search, version 2 with tests

--- column4-correct-programs/fast-binary-search/fast-binary-search.go
+++ column4-correct-programs/fast-binary-search/fast-binary-search.go
@@ -14,7 +14,32 @@ func fastSearch(a []int, v int) (p int) {
 	}
 
 	p = u
-	if u == n || a[u] != v {
+	if p == n || a[p] != v {
+		p = -1
+	}
+
+	return
+}
+
+//array with 10 elements
+func fastSearch2(a [10]int, v int) (p int) {
+	n := len(a)
+	i := 8 //largest power of two less than 10, tune depending on the size of the array
+	l := -1
+
+	if a[i] < v {
+		l = n - i
+	}
+
+	for i != 1 {
+		i /= 2
+		if a[l+i] < v {
+			l += i
+		}
+	}
+
+	p = l + 1
+	if p == n || a[p] != v {
 		p = -1
 	}
 
--- column4-correct-programs/fast-binary-search/fast-binary-search_test.go
+++ column4-correct-programs/fast-binary-search/fast-binary-search_test.go
@@ -14,12 +14,20 @@ type testData struct {
 	pos, v int
 }
 
+type testDataV2 struct {
+	array  [10]int
+	pos, v int
+}
+
+var array = []int{1, 5, 7, 8, 9, 12, 14, 22, 390, 434}
+var data = [10]int{1, 5, 7, 8, 9, 12, 14, 22, 390, 434}
+
 func TestFastSearchVersion1(t *testing.T) {
 	tests := []testData{
-		testData{array: []int{1, 5, 7, 8, 9, 12, 14, 22, 434}, v: 8, pos: 3},
-		testData{array: []int{1, 5, 7, 8, 9, 12, 14, 22, 434}, v: 434, pos: 8},
-		testData{array: []int{1, 5, 7, 8, 9, 12, 14, 22, 434}, v: 200, pos: -1},
-		testData{array: []int{1, 5, 7, 8, 9, 12, 14, 22, 434}, v: 1, pos: 0},
+		testData{array: array, v: 8, pos: 3},
+		testData{array: array, v: 434, pos: 9},
+		testData{array: array, v: 200, pos: -1},
+		testData{array: array, v: 1, pos: 0},
 		testData{array: []int{}, v: 0, pos: -1},
 		testData{array: []int{-5, -3, 0, 5}, v: -3, pos: 1},
 		testData{array: nil, v: 0, pos: -1},
@@ -27,6 +35,23 @@ func TestFastSearchVersion1(t *testing.T) {
 
 	for i, test := range tests {
 		if err := a.AssertDeepEqual(test.pos, fastSearch(test.array, test.v)); err != nil {
+			t.Error(m.ErrorMessageTestCount(i+1, err))
+		}
+	}
+}
+
+func TestFastSearchVersion2(t *testing.T) {
+	tests := []testDataV2{
+		testDataV2{array: data, v: 8, pos: 3},
+		testDataV2{array: data, v: 434, pos: 9},
+		testDataV2{array: data, v: 200, pos: -1},
+		testDataV2{array: data, v: 1, pos: 0},
+		testDataV2{array: [10]int{}, v: 10, pos: -1},
+		testDataV2{array: [10]int{-5, -3}, v: -3, pos: 1},
+	}
+
+	for i, test := range tests {
+		if err := a.AssertDeepEqual(test.pos, fastSearch2(test.array, test.v)); err != nil {
 			t.Error(m.ErrorMessageTestCount(i+1, err))
 		}
 	}
