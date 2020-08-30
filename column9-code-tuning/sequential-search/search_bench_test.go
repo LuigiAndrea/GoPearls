ddd02b60e9ac0f36cfd8a6b652d9a21dccd2cb0e
LAD <luigiandrea@gmail.com>
2020-08-29 22:52:02 -0400

    seperate benchmark column9

---
+++ column9-code-tuning/sequential-search/search_bench_test.go
@@ -0,0 +1,25 @@
+// +build column9_bench benchmark
+
+package searchtuning
+
+import (
+	"testing"
+)
+
+func BenchmarkSequentialSearchOriginal(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		seqSearchOriginal(i)
+	}
+}
+
+func BenchmarkSequentialSearch2(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		seqSearch2(i)
+	}
+}
+
+func BenchmarkSequentialSearch3(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		seqSearch3(i)
+	}
+}
--- column9-code-tuning/sequential-search/search_test.go
+++ column9-code-tuning/sequential-search/search_test.go
@@ -31,23 +31,4 @@ func TestSequentialSearch(t *testing.T) {
 			}
 		}
 	}
-
-}
-
-func BenchmarkSequentialSearchOriginal(b *testing.B) {
-	for i := 0; i < b.N; i++ {
-		seqSearchOriginal(i)
-	}
-}
-
-func BenchmarkSequentialSearch2(b *testing.B) {
-	for i := 0; i < b.N; i++ {
-		seqSearch2(i)
-	}
-}
-
-func BenchmarkSequentialSearch3(b *testing.B) {
-	for i := 0; i < b.N; i++ {
-		seqSearch3(i)
-	}
 }
