3972df7aa3267b1206de670b5921fe93e47e44ec
LAD <luigiandrea@gmail.com>
2020-09-25 19:30:14 -0400

    refactor sparse-vector tests using test-helper lib

--- column1-oyster/sparse-vector/sparse-vector_test.go
+++ column1-oyster/sparse-vector/sparse-vector_test.go
@@ -3,63 +3,65 @@
 package sparsevector
 
 import (
+	"errors"
 	"testing"
+
+	a "github.com/LuigiAndrea/test-helper/assertions"
+	m "github.com/LuigiAndrea/test-helper/messages"
 )
 
 func TestSparseVector(t *testing.T) {
 	sparseVector, _ := NewSparseVector(10)
-	expectedValues := map[int]int{3: 51, 2: 50, 9: 12, 8: 0}
-	expectedInitializedElement := len(expectedValues)
-	var numberOfInitializedElement int
+	expectedValues := map[int]int{3: 51, 2: 50, 9: 12, 7: 0, 8: 0}
+	expectedInitializedElements := len(expectedValues)
+	var numberOfInitializedElements int
 
-	sparseVector.add(3, 51)
-	sparseVector.add(2, 5)
-	sparseVector.add(9, 12)
-	sparseVector.add(2, 50)
-	sparseVector.add(8, 0)
+	for i, v := range expectedValues {
+		sparseVector.add(i, v)
+	}
 
 	for i := 0; i < sparseVector.length; i++ {
 		if num, err := sparseVector.get(i); err == nil {
-			numberOfInitializedElement++
-			if num != expectedValues[i] {
-				t.Errorf("Expected value %d - Actual value %d", expectedValues[i], num)
-			} else {
-				t.Logf("(Index: %d, Num: %d)", i, num)
+			numberOfInitializedElements++
+			if err := a.AssertDeepEqual(expectedValues[i], num); err != nil {
+				t.Error(m.ErrorMessageTestCount(i, err))
 			}
 		}
 	}
 
-	if numberOfInitializedElement != len(expectedValues) {
-		t.Errorf("Expected value %d - Actual value %d", expectedInitializedElement, numberOfInitializedElement)
-	} else {
-		t.Logf("%d elements have been initialized", numberOfInitializedElement)
+	if err := a.AssertDeepEqual(expectedInitializedElements, numberOfInitializedElements); err != nil {
+		t.Error(err)
 	}
 }
 
-func TestSparseVectorNegativeIndex(t *testing.T) {
-	indexesAdd := []int{10, -4, 3}
-	indexesGet := []int{12, 3, -4}
+func TestSparseVectorNegativeOutRangeIndexGetAndSet(t *testing.T) {
+	indexes := []int{12, 3, -4}
 
 	sparseVector, _ := NewSparseVector(3)
-	for _, v := range indexesAdd {
-		err := sparseVector.add(v, 100)
-		checkError(t, v, err)
+	for _, v := range indexes {
+		er := sparseVector.add(v, 100)
+		checkError(t, er)
 	}
 
-	for _, v := range indexesGet {
-		_, err := sparseVector.get(v)
-		checkError(t, v, err)
+	for _, v := range indexes {
+		_, er := sparseVector.get(v)
+		checkError(t, er)
 	}
-
-	idx := -3
-	_, err := NewSparseVector(idx)
-	checkError(t, idx, err)
 }
 
-func checkError(t *testing.T, idx int, err error) {
-	if err == nil {
-		t.Errorf("Expected index '%d' out of range", idx)
-	} else {
-		t.Log(err.Error())
+func TestSparseVectorElementNotInitialized(t *testing.T) {
+	sparseVector, _ := NewSparseVector(2)
+	_, er := sparseVector.get(1)
+	checkError(t, er)
+}
+
+func TestSparseVectorNegativeSparseVectorSize(t *testing.T) {
+	_, er := NewSparseVector(-3)
+	checkError(t, er)
+}
+
+func checkError(t *testing.T, er error) {
+	if err := a.AssertException(errors.New(""), er); err != nil {
+		t.Error(err)
 	}
 }
